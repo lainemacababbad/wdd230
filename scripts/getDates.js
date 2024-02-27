379d3e90a701fca1e6e2a08858681fa04128cc5c
lainemacababbad <149907223+lainemacababbad@users.noreply.github.com>
2024-02-27 14:25:18 -0700

    updated form

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -49,4 +49,34 @@ localStorage.setItem("numVisits-ls", numVisits);
 
 // 💡A client can view the localStorage data using the Applications panel in the browsers's DevTools - check it out on any major site.
 
+// START OF JS FOR FORM
+// password
+const p1 = document.querySelector("#password");
+const p2 = document.querySelector("#password2");
+const message = document.querySelector("#passwordmessage");
+
+p2.addEventListener("focusout", controlar);
+
+function controlar() {
+	if (p1.value !== p2.value) {
+		p1.value =""
+        p2.value=""
+        p1.focus()
+        message.textContent = "Password Do Not Match. Please try again."
+	} else {
+		message.textContent=""
+	}
+}
+
+// range
+const rangevalue = document.getElementById("rangevalue");
+const range = document.getElementById("rating");
+
+range.addEventListener('change', displayRatingValue);
+range.addEventListener('input', displayRatingValue);
+
+function displayRatingValue() {
+    rangevalue.innerHTML = range.value;
+}
+
   
