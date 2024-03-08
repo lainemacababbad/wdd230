53e0694c7901e1495fa3dd05cf341f010aed29a9
lainemacababbad <149907223+lainemacababbad@users.noreply.github.com>
2024-03-08 15:56:18 -0700

    css changes to prophets

--- week05/scripts/prophets.js
+++ week05/scripts/prophets.js
@@ -4,8 +4,8 @@ const cards = document.querySelector('#cards');
 const getProphetData = async () => {
   const response = await fetch(url);
   const data = await response.json();
-  console.table(data);
-  // displayProphets(data.prophets);
+  // console.table(data);
+  displayProphets(data.prophets);
 };
 
 getProphetData();
@@ -14,9 +14,11 @@ const displayProphets = (prophets) => {
   prophets.forEach((prophet) => {
     const card = document.createElement('section');
     const fullName = document.createElement('h2');
+    const birthInfo = document.createElement('p');
     const portrait = document.createElement('img');
 
     fullName.textContent = `${prophet.name} ${prophet.lastname}`;
+    birthInfo.textContent = `Date of Birth: ${prophet.birthdate}, Place of Birth: ${prophet.birthplace}`;
     portrait.setAttribute('src', prophet.imageurl);
     portrait.setAttribute('alt', `${prophet.name} ${prophet.lastname}`);
     portrait.setAttribute('loading', 'lazy');
@@ -24,6 +26,7 @@ const displayProphets = (prophets) => {
     portrait.setAttribute('height', '200');
 
     card.appendChild(fullName);
+    card.appendChild(birthInfo);
     card.appendChild(portrait);
 
     cards.appendChild(card);
