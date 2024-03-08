8322f677b2b7c9887c7c7ca1b18ccc7825830233
lainemacababbad <149907223+lainemacababbad@users.noreply.github.com>
2024-03-07 17:44:18 -0700

    fetch API

---
+++ week05/scripts/prophets.js
@@ -0,0 +1,31 @@
+const url = 'https://brotherblazzard.github.io/canvas-content/latter-day-prophets.json';
+const cards = document.querySelector('#cards');
+
+const getProphetData = async () => {
+  const response = await fetch(url);
+  const data = await response.json();
+  console.table(data);
+  // displayProphets(data.prophets);
+};
+
+getProphetData();
+
+const displayProphets = (prophets) => {
+  prophets.forEach((prophet) => {
+    const card = document.createElement('section');
+    const fullName = document.createElement('h2');
+    const portrait = document.createElement('img');
+
+    fullName.textContent = `${prophet.name} ${prophet.lastname}`;
+    portrait.setAttribute('src', prophet.imageurl);
+    portrait.setAttribute('alt', `${prophet.name} ${prophet.lastname}`);
+    portrait.setAttribute('loading', 'lazy');
+    portrait.setAttribute('width', '200');
+    portrait.setAttribute('height', '200');
+
+    card.appendChild(fullName);
+    card.appendChild(portrait);
+
+    cards.appendChild(card);
+  });
+};
